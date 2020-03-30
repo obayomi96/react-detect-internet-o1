a3ea927fa9efec9152b4083f47713bcafb8fdc34
martins obayomi <obayomimartins96@gmail.com>
2020-03-30 19:11:33 +0100

    React Detect Internet V 1.0.0

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import DetectInternet from './DetectInternet/DetectInternet';
+
+const App = () => {
+  return (
+    <div>
+      <DetectInternet
+        message="Not connected.. Please check your internet connection"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          color: "#fff",
+          background: "red",
+          textAlign: "center",
+          height: "3em",
+          alignItems: "center",
+          justifyContent: 'center',
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
